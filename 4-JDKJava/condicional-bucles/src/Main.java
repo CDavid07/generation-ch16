fb0c18f3bbc12b7a81abe4aeece39c737fbabf03
CDavid07 <davidarturo2009@gmail.com>
2022-08-19 17:19:54 -0500

    Condicionales con JAVA

---
+++ 4-JDKJava/condicional-bucles/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte condicion = 1;
+
+        if(condicion == 1){
+            System.out.println("Se cumple la condicion numero 1");
+        } else if(condicion == 2){
+            System.out.println("Se cumplió la condicion numero 2");
+        } else {
+            System.out.println("No se cumplio ninguna condicion. Ejecucion por defecto");
+        }
+    }
+}
