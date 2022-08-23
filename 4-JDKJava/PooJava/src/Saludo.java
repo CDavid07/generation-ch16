a506cb2331e11b1f913aa881332546bd87890437
CDavid07 <73976024+CDavid07@users.noreply.github.com>
2022-08-23 18:14:56 -0500

    Colecciones Map, diccionarios y POO

---
+++ 4-JDKJava/PooJava/src/Saludo.java
@@ -0,0 +1,14 @@
+public class Saludo {
+
+    //Atributos
+    String textoS = "Hola mundo Java oop";
+
+    //Metodos
+    void saludar (){
+        System.out.println("Hola Java OOP");
+    }
+    String saludar0(){
+        return textoS;
+    }
+
+}
